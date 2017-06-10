3089f4b6c5176e824e425a3c0b91d8ec30de19a7
Shawn Lee <Shawn Lee>
2017-06-11 00:42:43 +0900

    initial commit

--- app.py
+++ app.py
@@ -60,9 +60,9 @@ def webhook():
 def chooseMessage(message):
 	if chooseGreeting(message):
 		return chooseGreeting(message)
-	if message.lower().translate(None, message.punctuation) == "who are you" or message.lower().translate(None, message.punctuation) == "what are you":
+	if message.lower().translate(None, string.punctuation) == "who are you" or message.lower().translate(None, string.punctuation) == "what are you":
 		return "I am HAFS Newsbot, coded by Shawn Lee. I am designed to provide you with news, but I can't do that as of now."
-	if message.lower().translate(None, message.punctuation) in ["fuck", "shit", "fucking"]:
+	if message.lower().translate(None, string.punctuation) in ["fuck", "shit", "fucking"]:
 		return random.choice(["Hey! Don't swear!", "You know, it isn't okay to swear..."])
 
 	
@@ -71,13 +71,13 @@ def chooseMessage(message):
 def chooseGreeting(message):
 	GREETINGS_KEYWORDS = ["hello","hi","hey","sup","whats up","good morning","yo"]
 	GREETINGS_RESPONSES = ["Hello!","Hi!","What's up?","How's it going?","Hey!"]
-	#GREETINGS_KEYWORDS_K = ["안녕", "안녕하세요", "하이", "ㅎㅇ"]
-	#GREETINGS_RESPONSES_K = ["안녕하세요!"]
+	GREETINGS_KEYWORDS_K = ["안녕", "안녕하세요", "하이", "ㅎㅇ"]
+	GREETINGS_RESPONSES_K = ["안녕하세요!"]
 	for word in message.split():
 		if message.lower() in GREETINGS_KEYWORDS:
 			return random.choice(GREETINGS_RESPONSES)
-	#if message.translate(None, message.punctuation) in GREETINGS_KEYWORDS_K:
-	#	return random.choice(GREETINGS_RESPONSES_K)
+	if message.translate(None, string.punctuation) in GREETINGS_KEYWORDS_K:
+		return random.choice(GREETINGS_RESPONSES_K)
 	return False
 
 
