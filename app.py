25f654d094cbd3b058bead1bb91211c8825379a4
Shawn Lee <Shawn Lee>
2017-06-12 20:34:40 +0900

    initial commit

--- app.py
+++ app.py
@@ -69,10 +69,10 @@ def processMessage(message_text, sender_id):
     	send_news_message(sender_id,"cnn")
     elif message_text.lower() == "the new york times":
     	send_message(sender_id, "Giving you the latest news from " + message_text + "!")
-		send_news_message(sender_id,"nyt")
+	send_news_message(sender_id,"nyt")
     elif message_text.lower() == "espn":
     	send_message(sender_id, "Giving you the latest news from " + message_text + "!")
-		send_news_message(sender_id,"espn")
+	send_news_message(sender_id,"espn")
     elif message_text.lower() == "time":
     	send_message(sender_id, "Giving you the latest news from " + message_text + "!")
     	send_news_message(sender_id,"time")
